30984c54748649b7cbb3e854ee7f332dac6ac870
Scott Maxwell <33496996+scottwmwork@users.noreply.github.com>
2020-02-19 17:15:43 -0700

    finised recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,29 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  count = 0
+  first_val = list(ingredients.values())[0] // list(recipe.values())[0] 
+  current_min = []
+  for ing in recipe:
+    #Check if ingredient is available
+    if ing not in ingredients or ingredients[ing] < recipe[ing]:
+      return 0 
+    val = ingredients[ing] // recipe[ing]
+    
+    if count == 0:
+      current_min.append(val)
+    elif val < current_min[len(current_min) - 1]:
+      current_min.append(val)
+    
+    count += 1 
 
+    
+  batch_size = current_min[len(current_min) - 1]   
+  return batch_size
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'cheese': 10 }
+  ingredients = { 'milk': 198, 'butter': 52, 'cheese': 10 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
