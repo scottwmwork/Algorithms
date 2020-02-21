79fce6b12f8ba73217890b368d2974823a7cf51c
Scott Maxwell <33496996+scottwmwork@users.noreply.github.com>
2020-02-20 23:33:33 -0700

    finished rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,23 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def rock_paper_scissors(rounds):
 
+  options = ['rock', 'paper', 'scissors']
+  results = []
+  if rounds == 0:
+    return([[]])
+  elif rounds == 1:
+    return([['rock'], ['paper'], ['scissors']])
+  else:
+
+    for option in rock_paper_scissors(rounds-1):
+
+      results.append((option + [options[0]]))
+      results.append((option + [options[1]]))
+      resilts.append((option + [options[2]]))
+
+    return results
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
